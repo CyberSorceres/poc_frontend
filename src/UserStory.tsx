243f960f7a9e89499d5d4e6fcf3677eec0efa37f
sabrinacaniato <Caniatosabrina@gmail.com>
2024-03-05 14:12:05 +0100

    select quasi funziona

--- src/UserStory.tsx
+++ src/UserStory.tsx
@@ -1,22 +1,30 @@
-import { MDBCheckbox, MDBCol, MDBContainer, MDBInput, MDBSelect} from "mdb-react-ui-kit";
+import { MDBCheckbox, MDBCol, MDBContainer, MDBInput} from "mdb-react-ui-kit";
 import type { UserStory } from "./types/user_story";
-import {useMemo} from "react";
+import type { User } from "./types/user";
+import Select, { ValueType } from 'react-select';
+import React, { useCallback, useEffect, useMemo, useRef, useState, CSSProperties, SetStateAction } from "react";
 
 export default function ({
   userStory,
+  user,
   sendToParent,
 }: {
   userStory: UserStory;
+  user: User[];
   sendToParent: (isActive: boolean) => void;
 }) {
-  /*const data = useMemo(
-    () => [ {userStory?.user?.filter((u) => u.role=="dev" ).map((f) =>(
-      { text: f.name, value: f.name },
-    ))??[]}
-      
-    ],
-    []
-  );*/
+  const data= user?.filter((u) => u.role=="sviluppatore" ).map((u) => ({ value: u._id, label: u.name }));
+  console.log(data);
+  /*const data= [
+    { value: 'apple', label: 'Apple' },
+    { value: 'banana', label: 'Banana' },
+    { value: 'orange', label: 'Orange' },
+  ];*/
+  const [selectedOption, setSelectedOption] = useState<{ value: string; label: string; } | null>(null);
+
+  const handleChange = (selectedOption: ValueType<{ value: string; label: string; }>) => {
+    setSelectedOption(selectedOption);
+  }
   return (
     <>
       <MDBContainer>
@@ -27,24 +35,35 @@ export default function ({
           label={userStory.descript}
         />
       </MDBContainer>
-      <select className="chooseRole">
-        {userStory?.user?.filter((u) => u.role=="dev" ).map((f) =>(
+      {(import.meta.env.VITE_ROLE=="pm") ?
+      <>
+      <Select
+        value={selectedOption}
+        onChange={handleChange}
+        options={data}
+      />
+       {/*}
+      <MDBSelect
+          onValueChange={(e) => setCurrentValue(e)}
+          label="Assegna uno sviluppatore alla user story"
+          data={data}
+        />
+       
+      <form className="chooseRole">
+      <select id="assign" >
+        {user?.filter((u) => u.role=="sviluppatore" ).map((f) =>( 
           <>
-          <option>{f.name}</option>
+          <option key={f._id}>{f.name}</option>
           </>
         ))??[]}
         </select>
-       {/*} <MDBContainer style={{ width: "300px" }} className="mt-5">
-      {inputActive ? (
-        <MDBInput onBlur={inputOnBlur} inputRef={otherInputEl} label="Other" id="form1" type="text" />
-      ) : (
-        <MDBSelect
-          onValueChange={(e) => setCurrentValue(e)}
-          label="Example label"
-          data={data}
-        />
-      )}
-    </MDBContainer>{*/}
+        </form>{*/}
+         </>
+         :
+        <></>
+      }
+    
+      
       <div>
         {userStory?.feedback?.map((f) => (
           <>
--- src/types/user.ts
+++ src/types/user.ts
@@ -1,7 +1,7 @@
 import { Project } from "./project";
 import { UserStory } from "./user_story";
 
-interface User {
+export interface User {
   name: string;
   password: string;
   role: string;
